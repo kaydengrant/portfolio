73c43fc2e9bd72649033cdf87d39fcaed22a7f8f
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-05-11 13:21:52 -0700

    added different text

--- src/theme/text.ts
+++ src/theme/text.ts
@@ -1,46 +1,17 @@
-import { lightColors, darkColors } from "./colors";
-
-export const lightText = {
+export default {
   button:`
-    color: ${lightColors.bg};
-    font-size: 18px;
-    font-weight: 600;
+    font-family: 'Poppins';
+    font-size: 24px;
   `,
   h1:`
-    color: ${lightColors.lightBrown};
     font-size: 32px;
     font-weight: 600;
   `,
   h2:`
-    color: ${lightColors.bg};
     font-size: 24px;
     font-weight: 600;
   `,
   p:`
-    color: ${lightColors.lightBrown};
-    font-size: 15px;
-    font-weight: 600;
-  `
-}
-
-export const darkText = {
-  button:`
-    color: ${darkColors.bg };
-    font-size: 18px;
-    font-weight: 600;
-  `,
-  h1:`
-    color: ${darkColors.lightBrown};
-    font-size: 32px;
-    font-weight: 600;
-  `,
-  h2:`
-    color: ${darkColors.bg };
-    font-size: 24px;
-    font-weight: 600;
-  `,
-  p:`
-    color: ${darkColors.lightBrown};
     font-size: 15px;
     font-weight: 600;
   `
