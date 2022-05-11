f64f62dc454639796dc5ae43101b6edf214c03fe
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-05-11 13:20:30 -0700

    added NavBar

---
+++ src/components/Buttons/NavBar/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const NamedContainer = styled.div`
+  display: flex;
+  width: 380px;
+  justify-content: space-between;
+`
