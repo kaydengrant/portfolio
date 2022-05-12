e962e03772b413ffd521a20227126813fa1353fd
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-05-11 17:22:29 -0700

    updated GeneralButton

--- src/components/Buttons/GeneralButton/styles.ts
+++ src/components/Buttons/GeneralButton/styles.ts
@@ -1,28 +1,23 @@
 import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
-  height: 50px;
+  ${({theme}) => theme.text.button};
+  margin: ${({theme}) => theme.spacing.extraSmall}px;
+  padding: ${({theme}) => theme.spacing.small}px;
+  color: ${({theme}) => theme.off};
   background-color: ${({theme}) => theme.lightBrown};
   border-radius: ${({theme}) => theme.radius.small}px;
+  cursor: pointer;
   border: none;
   transition: transform .2s;
   box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.35);
   &:hover {
     transform: scale(1.1); 
     background-color: ${({theme}) => theme.off};
+    color: ${({theme}) => theme.bg};
   }
   &:hover:active {
     transform: scale(1.1) translateY(4px);
     filter: brightness(1.2);
-  }
- `
-  
-export const Title = styled.h2`
-  ${({theme}) => theme.text.button};
-  color: ${({theme}) => theme.off};
-  margin: 0;
-  padding: ${({theme}) => theme.spacing.extraSmall}px;
-  ${ButtonContainer}:hover & {
-    color: ${({theme}) => theme.bg};
   }
 `
