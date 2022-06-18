e897f88a5aa397f2213bf7b7a917432ba6e5e439
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-06-17 22:33:29 -0700

    added style as prop

--- src/components/Buttons/IconButton/index.tsx
+++ src/components/Buttons/IconButton/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { CSSProperties } from 'styled-components';
 
 import { ButtonContainer, Icon } from './styles';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   image?: React.FC<React.SVGProps<SVGElement>>
+  style?: CSSProperties
 }
 
-const IconButton: React.FC<Props> = ({ image, ...props }) => {
+const IconButton: React.FC<Props> = ({ image, style, ...props }) => {
   return (
-    <ButtonContainer {...props}>
+    <ButtonContainer {...props} style={style}>
       <Icon as={image}/>
     </ButtonContainer>
   )
