0e387d222ebfabe7efa6c216f86d6064e7b934a4
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-07-02 17:52:29 -0700

    project data moved to assets folder

---
+++ src/components/Cards/ProjectCards/assets/index.tsx
@@ -0,0 +1,51 @@
+interface Card {
+  title: string,
+  subtitle: string,
+  body: string,
+  img: HTMLImageElement
+}
+
+export const PROJECT_DATA = [
+  {
+    title: 'Ergo',
+    subtitle: '0',
+    body: 'Ergo is a task application focused on habit forming and stat tracking. Using TypeScript and React Native, this project served as a means to learn and practice Full Stack Development.',
+    img: 'https://www.allthebestpetcare.com/wp-content/uploads/2021/10/cbd-oil-cat-anxiety.jpg'
+  },
+  {
+    title: 'Ergo2',
+    subtitle: '1',
+    body: 'Using TypeScript and React Native, this project served as a means to learn and practice Full Stack Development.',
+    img: 'https://alleycatsandangels.org/wp-content/uploads/2019/07/big-gray-tabby-tire.jpg'
+  },
+  {
+    title: 'aoisnfanf',
+    subtitle: '2',
+    body: 'Fail fond pretty parties decisively keeps wholly branch but calling not garden bachelor whether ready. Made received add husbands make smart open point honoured kept other pretended part laughter.',
+    img: 'https://alleycatsandangels.org/wp-content/uploads/2019/07/big-gray-tabby-tire.jpg'
+  },
+  {
+    title: 'the third',
+    subtitle: '3',
+    body: 'Charm enjoy horrible. Welcomed engrossed earnestly. Worthy fine consisted noisier must insipidity performed few arise points however hand exposed confined sportsman tall extensive. Breeding sincerity other shortly. Call steepest when appetite express set.',
+    img: 'https://alleycatsandangels.org/wp-content/uploads/2019/07/big-gray-tabby-tire.jpg'
+  },
+  {
+    title: '04',
+    subtitle: '4',
+    body: 'Death provided affection. Unwilling can far sooner cousin advanced really learning insipidity stimulated. Cease cannot numerous breakfast avoid comfort forty  regular seeing call conviction green ladies pretty come continual. Without park well. Wooded bed disposal summer was greatly.',
+    img: 'https://alleycatsandangels.org/wp-content/uploads/2019/07/big-gray-tabby-tire.jpg'
+  },
+  {
+    title: '04123',
+    subtitle: '5',
+    body: 'Death provided affection. Unwilling can far sooner cousin advanced really learning insipidity stimulated. Cease cannot numerous breakfast avoid comfort forty  regular seeing call conviction green ladies pretty come continual. Without park well. Wooded bed disposal summer was greatly.',
+    img: 'https://alleycatsandangels.org/wp-content/uploads/2019/07/big-gray-tabby-tire.jpg'
+  },
+  {
+    title: '045125',
+    subtitle: '6',
+    body: 'Death provided affection. Unwilling can far sooner cousin advanced really learning insipidity stimulated. Cease cannot numerous breakfast avoid comfort forty  regular seeing call conviction green ladies pretty come continual. Without park well. Wooded bed disposal summer was greatly.',
+    img: 'https://alleycatsandangels.org/wp-content/uploads/2019/07/big-gray-tabby-tire.jpg'
+  }
+]
