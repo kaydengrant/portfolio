7c74cc722c9646a71694e1941b63b7d0a29eb787
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-07-03 20:12:05 -0700

    added max-width and margin

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -11,16 +11,13 @@ export const GlobalStyle = createGlobalStyle<{ theme: StyledTheme }>`
 `
   
 export const MainContainer = styled.div`
-  min-height: 100%;
-  min-width: 100%;
   height: 2050px;
   width: 100%;
+  max-width: 1600px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  top: 0;
-  left: 0;
-  right: 0;
 `
 
 export const Title = styled.text`
