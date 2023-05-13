3820fc16311c60de148e2fff0ae01cbd6994195d
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-05-13 14:35:20 -0700

    updated clickable hover color

--- src/components/Buttons/OutlineButton.tsx
+++ src/components/Buttons/OutlineButton.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 const OutlineButton: React.FC<Props> = ({ text, Tag }) => {
   return (
-    <button className='group px-3 py-2 border-2 border-white rounded-lg hover:border-green'>
-      <Tag className='group-hover:text-green'>{text}</Tag>
+    <button className='group px-3 py-2 border-2 border-white rounded-lg hover:border-lightGreen'>
+      <Tag className='group-hover:text-lightGreen'>{text}</Tag>
     </button>
   );
 };
