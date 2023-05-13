c015ba98d01130f4180348614b2bee389ddfc6a3
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-05-13 00:06:23 -0700

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css';
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
