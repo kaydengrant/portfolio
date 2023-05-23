3e4b87b85d69b540e67085f1ec04807cba552c53
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-05-23 12:54:57 -0700

    updated hero and profile blob (new design)

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-import Profile from '../components/Profile';
+import ProfileBlob from './ProfileBlob';
 import { openInNewTab } from '@/utils/utils';
 
 const HeroSection = () => {
   return (
-    <section className='flex flex-col-reverse items-center justify-between md:flex-row md:justify-between md:items-center'>
-      <div className='flex flex-col items-center max-w-min justify-between'>
-        <div className='flex flex-col items-end'>
-          <h1>{'<Kayden'}</h1>
-          <h1>{'Grant/>'}</h1>
-        </div>
-        <h2 className='text-opacity-0 text-transparent bg-clip-text bg-gradient-to-r from-green to-lightGreen my-5'>
-          Software Engineer
-        </h2>
-        <p>
-          {`Hey, my name is Kayden Grant, welcome to my portfolio! I'm a highly motivated and detail-oriented Software Engineer based in Seattle.`}
-        </p>
-      </div>
-      <div>
-        <Profile />
-        <div className='flex flex-row justify-center'>
-          <FaGithub
-            className='react-icons'
-            onClick={openInNewTab('https://github.com/kaydengrant')}
-          />
-          <FaLinkedin
-            className='react-icons'
-            onClick={openInNewTab('https://www.linkedin.com/in/kaydengrant/')}
-          />
-        </div>
+    <section className='flex flex-col items-center text-center'>
+      <ProfileBlob />
+      <h1 className='mt-10'>{'<Kayden Grant/>'}</h1>
+      <h2 className='text-opacity-0 text-transparent bg-clip-text bg-gradient-to-r from-green to-lightGreen my-5'>
+        Software Engineer based in Seattle
+      </h2>
+      <div className='flex flex-row justify-center'>
+        <FaGithub
+          className='react-icons'
+          onClick={openInNewTab('https://github.com/kaydengrant')}
+        />
+        <FaLinkedin
+          className='react-icons'
+          onClick={openInNewTab('https://www.linkedin.com/in/kaydengrant/')}
+        />
       </div>
     </section>
   );
