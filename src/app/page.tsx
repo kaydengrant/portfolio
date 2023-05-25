472023d0addc1cec44ec79c4a8129e79c4ddffab
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2023-05-24 20:13:00 -0700

    added about section with emoticons and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Navigation from '@/components/Navigation';
 import HeroSection from '@/components/HeroSection';
+import AboutSection from '@/components/AboutSection';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Navigation />
       <div className='content-container'>
         <HeroSection />
+        <AboutSection />
       </div>
     </>
   );
