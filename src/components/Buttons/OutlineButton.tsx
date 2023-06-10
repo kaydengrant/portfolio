7c73d0f0b098236d014beceb6fab8d14c0e8d424
Kayden Grant <hello@kaydengrant.io>
2023-06-10 01:54:15 -0700

    added icons to outline button

--- src/components/Buttons/OutlineButton.tsx
+++ src/components/Buttons/OutlineButton.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import { Icon } from '../Icon';
+
 interface Props {
   text: string;
   Tag: React.ElementType;
+  icon?: any;
 }
 
-const OutlineButton: React.FC<Props> = ({ text, Tag }) => {
+const OutlineButton: React.FC<Props> = ({ text, Tag, icon }) => {
   return (
-    <button className='group px-3 py-2 border-2 border-white rounded-lg hover:border-lightGreen'>
+    <button className='flex flex-row group px-3 py-2 border-2 border-white rounded-lg hover:border-lightGreen items-center'>
       <Tag className='group-hover:text-lightGreen'>{text}</Tag>
+
+      {icon && <span className='ml-6'>{React.createElement(icon)}</span>}
     </button>
   );
 };
