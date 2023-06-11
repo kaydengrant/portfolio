39cae2f1c8087d9bf6bd2b340d97978ce9d12cba
Kayden Grant <hello@kaydengrant.io>
2023-06-11 15:30:16 -0700

    changed description text

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,17 +5,15 @@ import ShuffleEmoticon from './ShuffleEmoticon';
 const AboutSection = () => {
   return (
     <section className='flex flex-col-reverse items-center md:flex-row md:justify-around'>
-      <div className='flex flex-col items-center justify-center max-w-[80%] md:max-w-[50%] mt-14 md:mt-0'>
+      <div className='flex flex-col items-center justify-center max-w-[80%] md:max-w-[45%] mt-14 md:mt-0'>
         <h2 className='text-gradient mb-5'>About me</h2>
         <p>
-          {`Hello, my name is Kayden. I'm a passionate Software Engineer who is *something about web3*. 
-          I like to solve real-world problems by developing creative solutions centered around user needs.
-          I also enjoy working in diverse teams as I find they produce the best and most
-          innovative ideas.`}
+          {`Welcome to my portfolio! I'm a dedicated Software Engineer who enjoys tackling real-world challenges  
+          through developing creative solutions centered around user needs. I value working in diverse teams as I  
+          believe they produce the best and most innovative ideas.`}
         </p>
         <p className='mt-3'>
-          {`In my freetime I'm likely playing my guitar or working on personal
-          programming projects.`}
+          {`In my freetime I'm likely hanging out with my cat or working on personal projects.`}
         </p>
       </div>
       <ShuffleEmoticon />
