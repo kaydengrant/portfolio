816171fc4e74b90a82083e6ac813b1cdfb4866bd
Kayden Grant <hello@kaydengrant.io>
2023-08-30 00:41:12 -0700

    fixed project images

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,3 +1,8 @@
+import TheDeliciousBrewLogo from '../../public/TheDeliciousBrewLogo.png';
+import AvenueInConsultingLogo from '../../public/AvenueInConsultingLogo.png';
+import GreenhouseGlobalStatusProjectImage from '../../public/greenhouseProjectImage.png';
+import PitGuruProjectImage from '../../public/pitguruProjectImage.png';
+
 export type DataProps = {
   key: number;
   title: string;
@@ -13,31 +18,31 @@ export const projectData: Array<DataProps> = [
     title: 'The Delicious Brew',
     description: 'Coffee e-commerce & blog',
     repoLink: 'https://github.com/kaydengrant/the-delicious-brew',
-    demoLink: 'https://the-delicious-brew.vercel.app/',
-    projectImage: '/TheDeliciousBrewLogo.png',
+    demoLink: 'https://thedeliciousbrew.vercel.app/',
+    projectImage: TheDeliciousBrewLogo,
   },
   {
     key: 1,
     title: 'Avenue In Consulting Website Design',
-    description: 'Worked with client to re-design company website',
+    description: 'Worked with client to re-design company website.',
     demoLink: 'https://avenueinconsulting.com/',
-    projectImage: '/AvenueInConsultingLogo.png',
+    projectImage: AvenueInConsultingLogo,
   },
   {
     key: 2,
     title: 'Greenhouse Global Status',
     description:
-      'A website for understanding global trends in regards to climate change using a heat-map of the world.',
+      'A tool for visualizing global trends in regards to climate change using a heat-map of the world.',
     repoLink: 'https://github.com/Greenhouse-Effect/greenhouse-global-status',
     demoLink:
       'http://greenhouse-global-status.s3-website-us-west-2.amazonaws.com/',
-    projectImage: '/greenhouseProjectImage.png',
+    projectImage: GreenhouseGlobalStatusProjectImage,
   },
   {
     key: 3,
     title: 'PitGuru',
     description:
       'A highly customizable racing overlay for racing simulation games.',
-    projectImage: '/pitguruProjectImage.png',
+    projectImage: PitGuruProjectImage,
   },
 ];
