a740321396b542f3cdc8f421bc0af1c6524c971a
Kayden Grant <hello@kaydengrant.io>
2023-11-29 23:00:53 -0800

    updated colors and added dark/light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,17 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     colors: {
-      white: '#FFF',
-      black: '#151515',
-      darkGray: '#212121',
+      white: '#EBEBEB',
+      offwhite: '#e6e2dc',
+      black: '#141414',
+      darkGray: '#282828',
+      green: '#53C69C',
+      darkGreen: '#358f80',
       gray: '#858585',
-      green: '#197E59',
-      lightGreen: '#53C69C',
-      red: '#CC0000',
+      error: '#CC0000',
     },
     extend: {
       screens: {
